e809cb9cb26d6299c0a84b4d8d851a4329420a40
amaljpc <65457669+amaljpc@users.noreply.github.com>
2023-05-11 22:59:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # https://github.com/amaljpc/educhat.git
 import streamlit as st
 from gpt4free import you
+ st.write(you)
 # simple request with links and details
 def fetch(content):
     response = ""
